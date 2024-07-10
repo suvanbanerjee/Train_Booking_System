c7083a86f418b90347c91eb7735ca6a6322fc4c4
suvanbanerjee <banerjeesuvan@gmail.com>
2024-07-10 21:27:38 +0530

    init commit

---
+++ Project/MainPage.java
@@ -0,0 +1,163 @@
+package dbms;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.SystemColor;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+public class MainPage {
+
+	public JFrame frame;
+	static String url = "jdbc:mysql://localhost:3306/mydb?useSSL=false";
+	static String uname = "root";
+	static String password = "1234";
+	static String u_id;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainPage window = new MainPage(u_id);
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainPage(String u_id) {
+		MainPage.u_id = u_id;
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setForeground(SystemColor.activeCaption);
+		frame.setBounds(100, 100, 516, 329);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JLabel lblWelcomeToOnline = new JLabel("Welcome to Ticketing System");
+		lblWelcomeToOnline.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcomeToOnline.setFont(new Font("Century Gothic", Font.BOLD, 20));
+		lblWelcomeToOnline.setBounds(61, 11, 379, 36);
+		frame.getContentPane().add(lblWelcomeToOnline);
+
+		JLabel label = new JLabel("Source");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		label.setBounds(50, 72, 81, 14);
+		frame.getContentPane().add(label);
+
+		JLabel lblDestination = new JLabel("Destination");
+		lblDestination.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblDestination.setBounds(286, 74, 90, 14);
+		frame.getContentPane().add(lblDestination);
+
+		JComboBox<String> comboBox = new JComboBox<>();
+		comboBox.setModel(new DefaultComboBoxModel<>(new String[]{"Mumbai", "Jaipur", "Ahmedabad", "Delhi","Kolkata","Chennai","Banglore","Hyderabad","Pune"}));
+		comboBox.setBounds(125, 70, 98, 22);
+		frame.getContentPane().add(comboBox);
+
+		JComboBox<String> comboBox_1 = new JComboBox<>();
+		comboBox_1.setModel(new DefaultComboBoxModel<>(new String[]{"Mumbai", "Jaipur", "Ahmedabad", "Delhi","Kolkata","Chennai","Banglore","Hyderabad","Pune"}));
+		comboBox_1.setBounds(389, 70, 98, 22);
+		frame.getContentPane().add(comboBox_1);
+
+		JLabel lblDate = new JLabel("Day");
+		lblDate.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblDate.setBounds(50, 119, 46, 14);
+		frame.getContentPane().add(lblDate);
+
+		JComboBox<String> comboBox_3 = new JComboBox<>();
+		comboBox_3.setModel(new DefaultComboBoxModel<>(new String[]{"Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday","Sunday"}));
+		comboBox_3.setBounds(125, 117, 98, 22);
+		frame.getContentPane().add(comboBox_3);
+
+		JLabel lblNoOfSeats = new JLabel("No of Passengers");
+		lblNoOfSeats.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNoOfSeats.setBounds(248, 117, 128, 18);
+		frame.getContentPane().add(lblNoOfSeats);
+
+		JSpinner spinner = new JSpinner();
+		spinner.setModel(new SpinnerNumberModel(1, 1, 5, 1));
+		spinner.setBounds(389, 118, 54, 20);
+		frame.getContentPane().add(spinner);
+
+		JButton btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+				new Login_page().frame.setVisible(true);
+			}
+		});
+		btnBack.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnBack.setBounds(125, 183, 89, 23);
+		frame.getContentPane().add(btnBack);
+
+		JButton btnBookTicket = new JButton("Proceed");
+		btnBookTicket.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ResultSet rs = null;
+				String src = comboBox.getSelectedItem().toString();
+				String dest = comboBox_1.getSelectedItem().toString();
+				String day = comboBox_3.getSelectedItem().toString();
+				String[] train_name;
+				int total;
+				int no = (Integer) spinner.getValue();
+				String nest_query = "SELECT Train_no FROM Station WHERE " + src + " = 1 AND " + dest + " = 1 AND End <> '" + src + "' AND " + day + " >= " + no;
+				String query = "SELECT Train_name FROM Train WHERE Train_no IN (" + nest_query + ")";
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					Connection conn = DriverManager.getConnection(url, uname, password);
+					Statement st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+					if (!src.equals(dest)) rs = st.executeQuery(query);
+					if (rs == null && src.equals(dest)) {
+						JOptionPane.showMessageDialog(frame, "Sorry no Trains are available");
+						frame.setVisible(false);
+						new MainPage(u_id).frame.setVisible(true);
+					} else {
+						rs.last();
+						total = rs.getRow();
+						rs.beforeFirst();
+						train_name = new String[total];
+						int i = 0;
+						while (rs.next()) {
+							train_name[i++] = rs.getString("Train_name");
+						}
+						frame.setVisible(false);
+						new PassengerDetails(u_id, train_name, no, src, dest, day).frame.setVisible(true);
+					}
+				} catch (Exception exmp) {
+					exmp.printStackTrace();
+				}
+			}
+		});
+		btnBookTicket.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnBookTicket.setBounds(261, 183, 115, 23);
+		frame.getContentPane().add(btnBookTicket);
+	}
+}
---
+++ Schema.sql
@@ -0,0 +1,82 @@
+CREATE TABLE `Users` (
+  `username` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `city` varchar(45) NOT NULL,
+  `contact` bigint(10) NOT NULL,
+  `age` int(10) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  PRIMARY KEY (`username`)
+)
+
+INSERT INTO `Users` VALUES ('suvan','Suvan Banerjee','DSCE','Banglore',123456789,19,'Male');
+
+CREATE TABLE `Logins` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  FOREIGN KEY (`username`) REFERENCES `Users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+
+INSERT INTO `Logins` VALUES ('admin','admin');
+
+CREATE TABLE `Passengers` (
+  `pnr` bigint(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `age` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `source` varchar(45) NOT NULL,
+  `destination` varchar(45) NOT NULL,
+  `train_no` bigint(10) DEFAULT NULL,
+  PRIMARY KEY (`pnr`),
+  FOREIGN KEY (`username`) REFERENCES `Users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (`train_no`) REFERENCES `Trains` (`train_no`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) AUTO_INCREMENT=123460
+
+CREATE TABLE `Stations` (
+  `train_no` bigint(10) NOT NULL,
+  `mumbai` tinyint(1) NOT NULL,
+  `ahmedabad` tinyint(1) DEFAULT NULL,
+  `jaipur` tinyint(1) DEFAULT NULL,
+  `delhi` tinyint(1) DEFAULT NULL,
+  `end_station` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`train_no`),
+  FOREIGN KEY (`train_no`) REFERENCES `Trains` (`train_no`) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+
+INSERT INTO `Stations` VALUES 
+(1021,1,0,1,1,'Delhi'),
+(1233,0,0,1,1,'Jaipur'),
+(1678,1,1,1,0,'Jaipur'),
+(2335,1,0,0,1,'Mumbai'),
+(4513,1,1,1,1,'Delhi'),
+(4566,0,1,1,0,'Jaipur'),
+(5677,0,1,1,1,'Delhi'),
+(5785,0,1,0,1,'Ahmedabad'),
+(6245,1,1,1,0,'Mumbai'),
+(9815,0,1,1,0,'Ahmedabad');
+
+CREATE TABLE `Trains` (
+  `train_no` bigint(10) NOT NULL,
+  `train_name` varchar(45) NOT NULL,
+  `source` varchar(45) NOT NULL,
+  `destination` varchar(45) NOT NULL,
+  `monday` int(11) DEFAULT '600',
+  `tuesday` int(11) DEFAULT '600',
+  `wednesday` int(11) DEFAULT '600',
+  `cost` int(11) DEFAULT '500',
+  PRIMARY KEY (`train_no`)
+)
+
+INSERT INTO `Trains` VALUES 
+(1021,'Duronto','Mumbai','Delhi',590,597,598,650),
+(1233,'Rajkalyan Express','Delhi','Jaipur',600,600,600,800),
+(1678,'Kalyani Express','Mumbai','Jaipur',600,597,600,800),
+(2335,'Nizauddin Express','Delhi','Mumbai',600,600,600,600),
+(4513,'Rajdhani Express','Mumbai','Delhi',600,597,600,1000),
+(4566,'Nazi Express','Ahmedabad','Jaipur',600,600,600,700),
+(5677,'Krishna Express','Ahmedabad','Delhi',600,600,600,699),
+(5785,'Gujarat Mail','Delhi','Ahmedabad',600,600,600,990),
+(6245,'Rawal Express','Jaipur','Mumbai',600,600,600,720),
+(9815,'Fazal','Jaipur','Ahmedabad',600,600,600,640);
